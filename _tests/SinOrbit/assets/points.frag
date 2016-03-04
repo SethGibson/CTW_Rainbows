a6571a7f8812108c9645bb0638bd08703d098aeb
Seth Gibson <seth.gibson1@gmail.com>
2016-03-04 13:37:19 -0800

    accumulation tests -sG

---
+++ _tests/SinOrbit/assets/points.frag
@@ -0,0 +1,9 @@
+#version 150
+uniform vec4 u_Color;
+
+out vec4 FragColor;
+
+void main()
+{
+	FragColor = u_Color;
+}
--- _tests/SinOrbit/src/SinOrbitApp.cpp
+++ _tests/SinOrbit/src/SinOrbitApp.cpp
@@ -13,7 +13,8 @@ struct ptcl {
 	ptcl(float pAmp, float pRad, float pWL, float pSpeed, float pSize) :
 		Amp(pAmp), Rad(pRad), WL(pWL), Speed(pSpeed), Size(pSize)
 	{
-		mTimer = randInt(-360, 360);
+		//mTimer = randInt(-360, 360);
+		mTimer = 0;
 	}
 
 	float Amp;
@@ -48,43 +49,52 @@ class SinOrbitApp : public App {
 	void draw() override;
 
   private:
-	void reset();
+	void reset(bool pShader);
 
 	bool  mStarted;
 	float mAlpha;
 
 	vector<ptcl>	mPtcls;
+	vec4			mColor;
+
+	gl::VboRef		mPointData;
+	gl::VboMeshRef	mPointMesh;
+	gl::GlslProgRef	mPointShader;
 };
 
 SinOrbitApp::SinOrbitApp() : mStarted(false)
 {
-	reset();
+	reset(true);
 	gl::clear(Color(0, 0, 0));
+
+	gl::enableAdditiveBlending();
 }
 
 void SinOrbitApp::mouseDown( MouseEvent event )
 {
 	if (mStarted)
-		reset();
+		reset(false);
 
 	mStarted = true;
-
-	gl::disableAlphaBlending();
-	gl::enableAlphaBlending();
 }
 
 void SinOrbitApp::update()
 {
 	if (mStarted) {
+		if (getElapsedFrames() % 300 == 0) {
+			reset(false);
+		}
 		for (auto &p : mPtcls) {
 			p.step();
 		}
+		mPointData->bufferData(mPtcls.size()*sizeof(ptcl), mPtcls.data(), GL_DYNAMIC_DRAW);
 	}
 }
 
 void SinOrbitApp::draw()
 {
 	if (mStarted) {
+		//gl::clear(Color::black());
 		gl::color(ColorA(0, 0, 0, mAlpha));
 		gl::drawSolidRect(Rectf(vec2(), getWindowSize()));
 
@@ -92,35 +102,52 @@ void SinOrbitApp::draw()
 
 		gl::pushModelMatrix();
 		gl::translate(getWindowCenter());
-
-		for (auto &p : mPtcls) {
-			p.draw();
+		gl::rotate(toRadians(-90.0f));
+		{
+			gl::ScopedGlslProg shdr(mPointShader);
+			mPointShader->uniform("u_Color", mColor);
+			gl::ScopedState pts(GL_PROGRAM_POINT_SIZE, true);
+			gl::draw(mPointMesh);
 		}
 
 		gl::popModelMatrix();
 	}
 }
 
-void SinOrbitApp::reset()
+void SinOrbitApp::reset(bool pShader)
 {
 	mAlpha = randFloat(0.01f, 0.1f);
-	auto minR = randFloat(200.0f, 300.0f);
-	auto maxR = randFloat(250.0f, 400.0f);
-	auto numPtcls = randInt(500, 2000);
+	auto w = randFloat(5.0f,10.0f);
+	auto maxR = randFloat(350.0f, 400.0f);
+	//auto numPtcls = randInt(500, 1000);
+	auto numPtcls = 1000;
 	mPtcls.clear();
-
 
 	for (int i = 0; i < numPtcls; ++i) {
 		mPtcls.push_back(
 			ptcl(
 				randFloat(0.2f, 0.8f),
-				randFloat(minR, maxR),
+				randFloat(400.0f-w, 400.0f+w),
 				randFloat(0.01f, 0.05f),
-				randFloat(0.3f, 1.1f),
+				randFloat(-0.5f, 0.5f),
 				randFloat(1.0f, 3.0f)
-				)
-			);
+			)
+		);
 	}
+
+	if (pShader) {
+		mPointShader = gl::GlslProg::create(loadAsset("points.vert"), loadAsset("points.frag"));
+
+		geom::BufferLayout attribs;
+		attribs.append(geom::POSITION, 2, sizeof(ptcl), offsetof(ptcl, Pos), 0);
+		mPointData = gl::Vbo::create(GL_ARRAY_BUFFER, mPtcls, GL_DYNAMIC_DRAW);
+		mPointMesh = gl::VboMesh::create(mPtcls.size(), GL_POINTS, { {attribs, mPointData} });
+	}
+	else {
+		mPointData->bufferData(mPtcls.size()*sizeof(ptcl), mPtcls.data(), GL_DYNAMIC_DRAW);
+	}
+
+	mColor = vec4(randVec3(), randFloat(0.001f, 0.025f));
 }
 
 void prepareSettings(App::Settings *pSettings)
